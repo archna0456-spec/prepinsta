1c67335c91866f783dc809ef39a41779b5a77d34
Archna <archna0456@gmail.com>
2026-04-10 21:44:44 +0530

    leap year or not

---
+++ Leap_year_or_no.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using name space std;
+int main(){
+    int year;
+    cout<<"Enter a year: ";
+    cin>>year;
+    if (year % 4==0 || year % 400 ==0 && year % 100 != 0){
+        cout<<year<<"Yes, it is a leap year "<<endl;
+    } 
+    else{
+        cout<<year<<"No, it is not a leap year"<<endl;
+    }
+    return 0;
+}
