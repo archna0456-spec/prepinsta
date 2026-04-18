29fcb0032481aa4b357b3350a05698be3febfd6f
Archna <archna0456@gmail.com>
2026-04-18 22:16:54 +0530

    factorial number

---
+++ Factorial_number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num = 6, fact = 1;
+    
+    // Factorial of negative number doesn't exist
+    // Read more here - https://www.quora.com/Is-the-factorial-of-a-negative-number-possible
+    if(num < 0)
+        cout << "Not Possible";
+    else
+    {
+        for(int i = 1; i <= num; i++)
+            fact = fact * i;
+    }
+    
+    cout << "Fact " << num << ": " << fact;
+}
+// Time complexity: O(N)
+// Space complexity: O(1)
