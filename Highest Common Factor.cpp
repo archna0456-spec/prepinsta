ea5ebc1277c460993806931fc6b74665031c309b
Archna <archna0456@gmail.com>
2026-05-04 23:14:15 +0530

    Highest Common Factor

--- Highest Common Factor.cpp
+++ Highest Common Factor.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 using namespace std;
-
-
 int main()
 {
     int num1 = 36, num2 = 60, hcf = 1;
@@ -11,10 +9,6 @@ int main()
         if(num1 % i == 0 && num2 % i == 0)
             hcf = i;
     }
-
     cout<<"HCF of "<<num1<<" and "<<num2<<" is "<<hcf;
-
     return 0;
-
-
 }
