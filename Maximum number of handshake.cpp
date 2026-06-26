103e30f25fe6883ca7e08508225bb7e7244d78ec
Archna <archna0456@gmail.com>
2026-06-26 19:36:10 +0530

    Max number of handshakes

--- Maximum number of handshake.cpp
+++ Maximum number of handshake.cpp
@@ -1,17 +1,13 @@
 // C++ program to find the maximum number of handshakesM
 #include<iostream>
 using namespace std; 
-
 int main()
 {
-
     //fill the code
     int num = 9;
 
     int total = num * (num-1) / 2; // Combination nC2
 
     cout<<"For "<<num<<" people there will "<<total<<" handshakes";
-
     return 0;
-
 }
